7ce25a8bfffbd5a38c1b93f1a95aba72c013da44
SnapDragon7410 <maliktahir0702@gmail.com>
2019-12-15 18:52:00 +0200

    nicegrill

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+os.system("git init")
+os.system("git remote add origin https://github.com/erenmetesar/NiceGrill.git")
+os.system("git config branch.master.remote origin && git config branch.master.merge refs/heads/master")
+os.system("git pull")
+os.system("pip install -r requirements.txt")
+config = open("config.py", "w")
+config.write("API_ID = " + os.envrion.get("API_ID", "1234") + "\nAPI_HASH = " + repr(os.environ.get("API_HASH", "1234")))
+file = open("client_secret.json", "w")
+file.write(os.environ.get("client_secret", ""))
+file.close()
+import nicegrill
