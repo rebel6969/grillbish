8207126027d171f9091c33c14bd5369415f19c50
SnapDragon7410 <maliktahir0702@gmail.com>
2019-12-15 19:18:39 +0200

    test

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ os.system("git config branch.master.remote origin && git config branch.master.me
 os.system("git pull")
 os.system("pip install -r requirements.txt")
 config = open("config.py", "w")
-config.write("API_ID = " + os.envrion.get("API_ID", "1234") + "\nAPI_HASH = " + repr(os.environ.get("API_HASH", "1234")))
+config.write("API_ID = " + os.environ.get("API_ID", "1234") + "\nAPI_HASH = " + repr(os.environ.get("API_HASH", "1234")))
 file = open("client_secret.json", "w")
 file.write(os.environ.get("client_secret", ""))
 file.close()
